9c85a2f2d0e7483e7ca91f2ae15628146ec229c5
jaykhans <jahongirnagmatov@gmail.com>
2022-11-09 00:37:32 +0500

    add some content in Product component

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,6 +1,7 @@
 import path from "path";
 import fs from "fs/promises";
 import Layout from "../../components/layout";
+import Image from "next/image";
 
 async function getData() {
   const filePath = path.join(process.cwd(), "backend", "productsData.json");
@@ -43,12 +44,49 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Product({foundItem}) {
+export default function Product({ foundItem }) {
   return (
     <Layout>
-        <div>
-            <h1 className="text-xl">{foundItem.name}</h1>
+      <div className="w-full pl-4 pr-8 mt-8 mb-8">
+        <div className="bg-rd-bg px-5 py-4 rounded-lg">
+          <p className="text-gray-400">
+            Заявки &gt; <span className="text-rd-primary">Оформить заказ</span>
+          </p>
+
+          <div className="bg-white my-6 py-6 px-8">
+            <h1 className="text-rd-blue text-2xl font-semibold ">
+              {foundItem.fullName}
+            </h1>
+
+            <div className="grid grid-cols-2 gap-x-4 my-6">
+              <div className="bg-rd-bg p-4 rounded-[12px]">
+                <div className="text-center">
+                  <Image
+                    src={`/images/${foundItem.image}.png`}
+                    height={200}
+                    width={200}
+                    alt=""
+                    className="my-10 inline"
+                  />
+                </div>
+                <div className="text-center">
+                  <Image 
+                    src="/images/dots.svg" 
+                    height={60}
+                    width={60}
+                    alt=""
+                    className="inline"
+                    />
+                </div>
+              </div>
+
+              <div>
+                
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </Layout>
   );
 }
