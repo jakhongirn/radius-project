e4b42d15ff5a1666b2f1c16906547fa904a08e4b
jaykhans <jahongirnagmatov@gmail.com>
2022-11-09 15:38:12 +0500

    change import in index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Layout from "../components/Layout.jsx";
+import Layout from "../components/layout.jsx";
 import Checkout from "./checkout.js";
 
 export default function Home() {
