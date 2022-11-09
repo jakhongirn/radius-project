1c57d46fe777b00855f2972a0fd2c2f80811496e
jaykhans <jahongirnagmatov@gmail.com>
2022-11-09 11:57:38 +0500

    readd files

---
+++ components/TopNavbar.jsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const TopNavbar = () => {
+  return (
+    <div className="w-full bg-white">
+      <div className="flex justify-between items-center mx-12 my-4">
+        <div className="">
+          <Link href="/">
+            <Image
+              src="/images/logo-radius.webp"
+              width={90}
+              height={64}
+              alt="logo radius"
+            />
+          </Link>
+        </div>
+
+        <div className="flex gap-x-8">
+          <div>
+            <Image
+              src="/images/notification.svg"
+              className=""
+              width={28}
+              height={28}
+              alt=""
+            ></Image>
+          </div>
+          <div className="bg-rd-blue -my-4 py-4 -mr-12 px-8 rounded-bl-[2.5rem]">
+            <h2 className="text-lg text-white">
+              Личный кабинет
+              <Image
+                src="/images/cabinet.svg"
+                className="inline ml-2"
+                width={32}
+                height={32}
+                alt=""
+              />
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TopNavbar
